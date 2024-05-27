383c00721eef9514430b4dcee2c06452d355707b
Paul Uchenna <pauluchenna20@gmail.com>
2024-05-27 17:01:18 +0100

    Update p-shop

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -7,6 +7,7 @@ import Products from "./pages/Products";
 import Cart from "./pages/Cart";
 import LoginSignup from "./pages/LoginSignup";
 import Footer from "./component/Footer/Footer";
+import NotFound from "./pages/NotFound"; // Create this component for 404 errors
 import men_banner from "./component/assets/banner_mens.png";
 import women_banner from "./component/assets/banner_women.png";
 import kid_banner from "./component/assets/banner_kids.png";
@@ -35,6 +36,8 @@ function App() {
           </Route>
           <Route path="/cart" element={<Cart />} />
           <Route path="/login" element={<LoginSignup />} />
+          {/* Catch-all route for 404 */}
+          <Route path="*" element={<NotFound />} />
         </Routes>
         <Footer />
       </BrowserRouter>
